5c278d677168f53ee96035e381ef4db508320776
Alex Fallah <fallaha@wit.edu>
2016-07-14 11:27:12 -0400

    Median selection part 1

--- MedianSelection.playground/Contents.swift
+++ MedianSelection.playground/Contents.swift
@@ -48,4 +48,54 @@ func random(start: Int, to end: Int) -> Int {
 quicksortRandom(&list, low: 0, high: list.count-1)
 
 
+func randomStringWithLength (len : Int) -> NSString {
+    
+    let letters : NSString = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+    
+    var randomString : NSMutableString = NSMutableString(capacity: len)
+    
+    for (var i=0; i < len; i++){
+        var length = UInt32 (letters.length)
+        var rand = arc4random_uniform(length)
+        randomString.appendFormat("%C", letters.characterAtIndex(Int(rand)))
+    }
+    
+    return randomString
+}
+
+
+let values = [["checked" : false], ["ID" : ""]]
+
+
+for (x,y) in values.enumerate() {
+  
+    print(" \(x) and ,\(y)")
+}
+//Input: Subarray A[l..r] of array A[0..n-1] of orderable elements and //integer k (1 ≤ k ≤ r-l+1)
+func quickSelect(inout A: [Int], k: Int) -> Int {
+    
+    let s = partitionLomuto(&A, low: 0, high: A.count-1)
+    
+    return 0
+    
+}
+
+
+//ALGORITHM QuickSelect(A[l..r], k)
+////Input: Subarray A[l..r] of array A[0..n-1] of orderable elements and //integer k (1 ≤ k ≤ r-l+1)
+////Output: The value of the k-th smallest element in A[l..r]
+//s <- LomutoPartition(A[l..r])
+//if s = k-1
+//return A[s]
+//else if s > l+k-1
+//QuickSelect(A[l..s-1], k)
+//else
+//QuickSelect(A[s+1..r], k-1-s)
+//
+
+randomStringWithLength(10)
+randomStringWithLength(10)
+randomStringWithLength(10)
+
+
 
